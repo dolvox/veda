fc5c2835132452bc1e95485de70bcea8cca51ea4
dolvox <dolvoxx@gmail.com>
2026-07-01 11:36:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Inicialização e Setup de Animações
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Confirmação de que o GSAP carregou e o DOM está pronto
+    console.log("Esqueleto inicializado. GSAP versão:", gsap.version);
+
+    // As animações das dobras serão registradas aqui
+});
